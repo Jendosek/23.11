57e73ab6ea401f8f9ded0eb76dfbb80adcdef47c
WW <abc123@gmail.com>
2024-11-23 13:50:42 +0200

    practical1

---
+++ Entities/Rebuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1.Entities
+{
+    internal class Rebuilder
+    {
+        public static void Rebuild(string line)
+        {
+            int[] numbers = new int[9];
+            int count = 0;
+
+            Console.WriteLine("Введіть 9 чисел від 0 до 9:");
+            while (count < 9)
+            {
+                try
+                {
+                    Console.Write($"Введіть число {count + 1}: ");
+                    string input = Console.ReadLine();
+
+                    int number = int.Parse(input);
+
+                    if (number < int.MinValue || number > int.MaxValue)
+                    {
+                        throw new OverflowException();
+                    }
+
+                    numbers[count] = number;
+                    count++;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Помилка: введене значення не є числом. Спробуйте ще раз.");
+                }
+            }
+
+            Console.WriteLine("Введені числа:");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using ConsoleApp1.Entities;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -9,6 +10,7 @@ namespace ConsoleApp1
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
+            Rebuilder.Rebuild(Console.ReadLine());
         }
     }
 }
