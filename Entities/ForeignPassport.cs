4e8f26cb3bec694fd86e75d90a6b240a8129d2ad
Jendosek <bala01081977@gmail.com>
2024-11-24 00:49:34 +0200

    hm3

---
+++ Entities/ForeignPassport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entities
+{
+    internal class ForeignPassport
+    {
+        private string _id { get; set; }
+        public string Name { get; set; }
+        private string _date { get; set; }
+
+        public Id
+        {
+            get
+            {
+                return new Id(_id);
+            }
+            set
+            {
+                _id = value.Value;
+            }
+        }
+
+
+    }
+}
